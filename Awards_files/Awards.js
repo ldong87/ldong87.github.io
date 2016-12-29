a713352871e9b69fa153ab1d3eac6275097fa008
ldong87 <ldong87@gmail.com>
2016-12-29 15:03:44 -0500

    move webpages to github

---
+++ Awards_files/Awards.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20161227
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Awards_files/AwardsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
